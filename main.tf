374298d41790331e4789987f5967d79079ee8477
Gabriel Filippi <gabriel._gf@hotmail.com>
2025-01-17 18:07:57 -0300

    deploy com terraform e ansible

--- main.tf
+++ main.tf
@@ -17,6 +17,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
   key_name = "curso-alura-iac"
+  # user_data = <<-EOF
+  #               #!/bin/bash
+  #               cd /home/ubuntu
+  #               echo "<h1>Feito com Terraform</h1> > index.html
+  #               nohup busybox httpd -f -p 8080 &
+
+  #                EOF 
   tags = {
     Name = "Curso Alura IaC"
   }
